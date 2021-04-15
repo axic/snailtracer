10a0f4bbc2cf937e10c61992b6f0e368b32ff7f8
Alex Beregszaszi <alex@rtfs.hu>
2021-04-15 13:28:41 +0100

    Support Solidity 0.6.x and 0.7.x

--- snailtracer.sol
+++ snailtracer.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.8.0;
 contract SnailTracer {
   // Image properties for the path tracer
   int    width;  // Width of the image being generated, fixed for life
